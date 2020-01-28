518554af412327f61d6b28c81bc6df3a51fc7d1b
beata <beata@emc2wave.com>
2020-01-28 15:29:10 -0800

    add catch()

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-fetch("https://swapi.co/api/planets/").then(response => {
-  response.json().then(data => {
-    for (let planet of data.results) {
-      console.log(planet.name);
-    }
+fetch("https://swapi.co/api/planets/")
+  .then(response => {
+    response.json().then(data => {
+      for (let planet of data.results) {
+        console.log(planet.name);
+      }
+    });
+  })
+  .catch(err => {
+    console.log("SOMETHING WENT WRONG WITH FETECH");
+    console.log(err);
   });
-});
