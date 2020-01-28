0fff337f3d8980335edd0103a56b1d7c928d8ec2
beata <beata@emc2wave.com>
2020-01-28 15:38:59 -0800

    add error catching to response

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 fetch("https://swapi.co/api/planets/")
   .then(response => {
-    response.json().then(data => {
-      for (let planet of data.results) {
-        console.log(planet.name);
-      }
-    });
+    if (!response.ok) {
+      console.log("ERROR NOT STATUS 200", response.status);
+    } else {
+      response.json().then(data => {
+        for (let planet of data.results) {
+          console.log(planet.name);
+        }
+      });
+    }
   })
   .catch(err => {
     console.log("SOMETHING WENT WRONG WITH FETECH");
