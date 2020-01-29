95831126bea326f37b127fc7246cbd0d4b7ee46c
beata <beata@emc2wave.com>
2020-01-28 17:00:26 -0800

    create printPlanets()

--- app.js
+++ app.js
@@ -6,28 +6,29 @@ const checkStatusAndParse = response => {
   }
 };
 
+const printPlanets = data => {
+  console.log("FETCHED ALL PLANETS(first 10)");
+  for (let planet of data.results) {
+    console.log(planet.name);
+  }
+  // const p = new Promise((resolve, reject) => {
+  //   resolve(data);
+  // });
+  // return p;
+
+  return Promise.resolve(data);
+};
+
 fetch("https://swapi.co/api/planets/")
   .then(checkStatusAndParse)
+  .then(printPlanets)
   .then(data => {
-    console.log("FETCHED ALL PLANETS(first 10)");
-    for (let planet of data.results) {
-      console.log(planet.name);
-    }
     const nextURL = data.next;
     return fetch(nextURL);
   })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`Status code Error: ${response.status}`);
-    } else {
-      return response.json();
-    }
-  })
+  .then(checkStatusAndParse)
+  .then(printPlanets)
   .then(data => {
-    console.log("FETCHED NEXT 10 PLANETS");
-    for (let planet of data.results) {
-      console.log(planet.name);
-    }
     const nextURL = data.next;
     return fetch(nextURL);
   })
