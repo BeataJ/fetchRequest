21e47da840e073ad2e004a79aade16432e78c139
beata <beata@emc2wave.com>
2020-01-28 16:08:58 -0800

    add deeper http fetch

--- app.js
+++ app.js
@@ -7,7 +7,18 @@ fetch("https://swapi.co/api/planets/")
     }
   })
   .then(data => {
-    console.log(data.results[0].films[0]);
+    const filmUrl = data.results[0].films[0];
+    return fetch(filmUrl);
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Status code Error: ${response.status}`);
+    } else {
+      return response.json();
+    }
+  })
+  .then(data => {
+    console.log(data);
   })
   .catch(err => {
     console.log("SOMETHING WENT WRONG WITH FETCH");
