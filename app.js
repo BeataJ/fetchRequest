303599ae1dc3345559bb87f2afb970c27606636b
beata <beata@emc2wave.com>
2020-01-28 16:32:52 -0800

    fetch 20 planets

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ fetch("https://swapi.co/api/planets/")
     }
   })
   .then(data => {
-    console.log("FETCHED ALL PLANETS");
-    const filmUrl = data.results[0].films[0];
-    return fetch(filmUrl);
+    console.log("FETCHED ALL PLANETS(first 10)");
+    for (let planet of data.results) {
+      console.log(planet.name);
+    }
+    const nextURL = data.next;
+    return fetch(nextURL);
   })
   .then(response => {
     if (!response.ok) {
@@ -19,8 +22,12 @@ fetch("https://swapi.co/api/planets/")
     }
   })
   .then(data => {
-    console.log("FETCHED FIRST FILM< BASE OFF FIRST PLANET");
-    console.log(data.title);
+    console.log("FETCHED NEXT 10 PLANETS");
+    for (let planet of data.results) {
+      console.log(planet.name);
+    }
+    const nextURL = data.next;
+    return fetch(nextURL);
   })
   .catch(err => {
     console.log("SOMETHING WENT WRONG WITH FETCH");
